8ac9848cc92e25dfe6ad5e7f6b543af14488de69
Salik Ahmed Khan <salik.khan@aciano.net>
2020-03-05 15:28:44 +0500

    Revert "amendment 2"
    This reverts commit 503cdece8104d7c6a62065fbcc81752158ba3a89.

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -2,10 +2,10 @@
   window.extractData = function() {
     var ret = $.Deferred();
 
-    //function onError() {
-    //  console.log('Loading error', arguments);
-    //  ret.reject();
-    //}
+    function onError() {
+      console.log('Loading error', arguments);
+      ret.reject();
+    }
 
     //function onReady(smart)  {
     //  if (smart.hasOwnProperty('patient')) {
